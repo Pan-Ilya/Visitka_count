523fe6b16e42aadceb15f4c478b0311e096bcdb0
Ilya Pankratov <pankratovilyasergeevich@gmail.com>
2023-01-25 03:28:18 +0200

    Fixed RegEx for filename. Now correctly read post process 'SVERL' for example.

--- patterns.py
+++ patterns.py
@@ -1,10 +1,9 @@
-right_filename_pattern = r'(?i)' \
-                         r'(?P<date>\d{2}-\d{2}).*?' \
-                         r'(?P<OrderID>\d+)_' \
-                         r'(?P<size>\d+[xх]\d+)_' \
-                         r'(?P<color>\d\+\d).*?' \
-                         r'(?P<density>\d{3})_' \
-                         r'(?P<lam>[a-z]{2,3}\d\+\d)?.*?' \
+right_filename_pattern = r'(?i)(?P<date>\d{2}-\d{2})_.*?' \
+                         r'(?P<OrderID>\d+)_.*?' \
+                         r'(?P<size>\d+[xх]\d+)_.*?' \
+                         r'(?P<color>\d\+\d)_.*?' \
+                         r'(?P<density>\d{3})_.*?' \
+                         r'(?:(?P<lam>[a-z]{2,3}\d\+\d)|.)*?' \
                          r'(?P<quantity>[\d ]{3,}).*?' \
                          r'(?P<file_format>\.pdf)'
 
